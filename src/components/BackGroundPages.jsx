bedd1b7f924d9409fe704f40f34cebd4b000914a
Pini Clavero <maxi.a.clavero@gmail.com>
2023-10-04 19:20:55 -0300

    pages backgraund

---
+++ src/components/BackGroundPages.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import "../../styles/backGroundPages.css"
+
+function BackGroundPages() {
+    return (
+        <div class="backgroundPages">
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+</div>
+    )
+}
+
+export default BackGroundPages 
