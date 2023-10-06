73e486e33948e6cdb7e0a03866895a65c3297ad0
AXaxelclavero <axel.m.clavero@gmail.com>
2023-10-06 16:22:49 -0300

    instagram realizado

--- src/pages/biografia.jsx
+++ src/pages/biografia.jsx
@@ -6,7 +6,11 @@ const Biografia = () => {
       <h1 className='biografia_titulo'>SOBRE MI</h1>
       <div className='biografia_flex'>
    <p className='biografia_parrafo'>Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las</p>
-      <img className='biografia_imagen' src="/galeria1_dance.PNG" alt="dance imagen" />    </div>
+      {/* <img className='biografia_imagen' src="/galeria1_dance.PNG" alt="dance imagen" />  */}
+      <iframe className='biografia_instagram' src="https://www.instagram.com/beluudimartino/embed/" width="350" height="450" frameborder="0"  scrolling="yes"></iframe>
+
+      
+        </div>
       </div>
    
   )
